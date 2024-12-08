80e720fe552e6d0ff3642ca8b0d82ff278c04e14
baishyaRahul <baishyarahul94@gmail.com>
2024-12-08 11:17:38 +0530

    updated endpoint 1

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(cors());
 
 //Endpoint 1: Calculate the total price of items in the cart
 function calculateCartValue(newItemPrice, cartTotal) {
-  let totalValue = newItemPrice + cartTotal;
+  let totalItemPrice = newItemPrice + newItemPrice + newItemPrice;
+  let totalValue = totalItemPrice + cartTotal;
   return totalValue.toString();
 }
 app.get('/cart-total', (req, res) => {
